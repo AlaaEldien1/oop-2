5333e969dbdc4ade06dc1c1c82a761fe267da8db
AlaaEldien1 <alaaabdalla0123@gmail.com>
2025-07-16 01:09:10 +0300

    Add project files.

---
+++ oop-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region question1
+            // 1- Design and implement a Class for the employees in a company:
+            //Employee employee = new Employee(1,"ali",SecurityLevel.guest,200, DateTime.Now,'m');
+            //Console.WriteLine(employee.ToString());
+            #endregion
+            #region question2
+            //Develop a Class to represent the Hiring Date Data:Consisting of fields to hold the day, month and Years.
+
+            //HiringDate now = new HiringDate(1,1,2020);
+            //Console.WriteLine(now.ToString());
+
+            #endregion
+            #region question3
+            //Employee[] EmpArr = new Employee[3];
+
+            //EmpArr[0] = new Employee(1,"Ahmed",SecurityLevel.DBA,15000, new DateTime(2022, 4, 10),'m' );
+
+            //EmpArr[1] = new Employee(2,"Sara",SecurityLevel.guest,3000,new DateTime(2023, 2, 5),'f');
+
+            //EmpArr[2] = new Employee(3,"Kareem",SecurityLevel.secretary,8000,new DateTime(2024, 1, 20),'m');
+            //foreach (Employee emp in EmpArr)
+            //{
+            //    Console.WriteLine(emp);
+            //}
+            #endregion
+        }
+    }
+}
---
+++ oop-2/employee.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2
+{
+    public enum SecurityLevel {
+        guest,
+        developer,
+        secretary,
+        DBA
+    }
+    internal class Employee
+    {
+        private int id;
+        private string name;
+        private SecurityLevel security;
+        private int salary;
+        private DateTime hireDate;
+        private char gender;
+        public char Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == 'm' || value == 'f')
+                    gender = value;
+                else
+                    Console.WriteLine("enter m for male or f for female");
+            }
+        }
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    name = value;
+                else
+                    Console.WriteLine("name canot be empty");
+            }
+        }
+        public SecurityLevel Security
+        {
+            get { return security; }
+            set
+            {
+                if (Enum.IsDefined(typeof(SecurityLevel), value))
+                    security = value;
+                else
+                    Console.WriteLine("enter one of the security levels");
+            }
+
+        }
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        public DateTime HireDate
+        {
+            get { return hireDate; }
+            set { hireDate = value; }
+        }
+        public Employee(int id, string name, SecurityLevel security, int salary, DateTime hireDate,char gender)
+        {
+            Id = id;
+            Name = name;
+            Security = security;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = gender;
+        }
+        public Employee()
+        {
+        }
+        public override string ToString()
+        {
+            return $"Id : {id} name : {name } salary {String.Format("{0:C}", Salary)} security level : {security} hire date {hireDate} gender {gender}";
+        }
+    }
+}
